b60104586fa06a48adb809e03167fd0ca5038c5a
eugene0204 <eugene204@gmail.com>
2015-03-12 03:21:03 +0900

    2015-03-11

---
+++ Businesses/BusinessUtils.java
@@ -0,0 +1,13 @@
+package siva.borie.Businesses;
+
+/**
+ * Created by Eungjun on 2015-03-12.
+ */
+public class BusinessUtils
+{
+    public static final String BIZ_NAME = "biz_name";
+    public static final String BIZ_ADDRESS = "biz_address";
+
+    public static final String OWNER_EMAIL = "owner_email";
+
+}
--- Businesses/fragments/RecommendedServiceFragment.java
+++ Businesses/fragments/RecommendedServiceFragment.java
@@ -1,5 +1,6 @@
 package siva.borie.Businesses.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,8 +8,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -16,10 +18,13 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 import siva.borie.Businesses.Business;
+import siva.borie.Businesses.BusinessUtils;
 import siva.borie.R;
 import siva.borie.network.NetworkController;
 
@@ -32,6 +37,8 @@ public class RecommendedServiceFragment extends Fragment
     private final String URL = "http://192.168.0.1:8080/biz/recommendlist";
 
     private ListView mListView;
+    private ListViewAdapter mAdapter;
+    private ArrayList<Business> mBusinessArrayList;
 
 
     @Override
@@ -40,13 +47,16 @@ public class RecommendedServiceFragment extends Fragment
         View root = inflater.inflate(R.layout.fragment_recommended_service, container,false );
 
         mListView = (ListView) root.findViewById(R.id.recommended_service_listview);
+        mBusinessArrayList = new ArrayList<Business>();
+        mAdapter = new ListViewAdapter(getActivity(), -1);
+        mListView.setAdapter(mAdapter);
 
-        getList();
+        setListRequest();
 
         return root;
     }
 
-    private void getList()
+    private void setListRequest()
     {
         Log.d(TAG, "getList");
         JsonArrayRequest req = new JsonArrayRequest(URL, new Response.Listener<JSONArray>()
@@ -55,6 +65,9 @@ public class RecommendedServiceFragment extends Fragment
             public void onResponse(JSONArray jsonArray)
             {
                 Log.d(TAG, jsonArray.toString());
+
+                getResponse(jsonArray);
+                mAdapter.notifyDataSetChanged();
 
             }
         }, new Response.ErrorListener()
@@ -68,43 +81,96 @@ public class RecommendedServiceFragment extends Fragment
 
         NetworkController.getInstance(getActivity().getApplicationContext()).addToRequestQueue(req);
 
-        Log.d(TAG,"end of getList");
+    }
+
+    private void getResponse(JSONArray _jsonArray)
+    {
+       JSONArray jsonArray = _jsonArray;
+       int lenght = jsonArray.length();
+
+       for(int i = 0; i < lenght; ++i)
+       {
+           Business biz  = new Business();
+
+           try
+           {
+               JSONObject json = (JSONObject) jsonArray.get(i);
+
+               biz.setmName(json.getString(BusinessUtils.BIZ_NAME));
+               biz.setmAddress(json.getString(BusinessUtils.BIZ_ADDRESS));
+               biz.setmEmail(json.getString(BusinessUtils.OWNER_EMAIL));
+
+               mBusinessArrayList.add(biz);
+
+           } catch (JSONException e)
+           {
+
+               Log.d(TAG, e.toString());
+           }
+       }
     }
 
 
 
-    private class listViewAdapter extends BaseAdapter
+    private class ListViewAdapter extends ArrayAdapter
     {
-        private final ArrayList<Business> arrayList;
 
-        private listViewAdapter(final ArrayList<Business> arrayList)
+        public ListViewAdapter(Context context, int resource)
         {
-            this.arrayList = arrayList;
+            super(context, resource);
         }
-
 
         @Override
         public int getCount()
         {
-            return 0;
+            return mBusinessArrayList.size();
         }
 
         @Override
         public Object getItem(int position)
         {
-            return null;
+            return mBusinessArrayList.get(position);
         }
 
         @Override
         public long getItemId(int position)
         {
-            return 0;
+            return position;
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent)
+        public android.view.View getView(int position, android.view.View convertView, ViewGroup parent)
         {
-            return null;
+            ViewHolder viewHolder;
+
+            if(null == convertView)
+            {
+                convertView = View.inflate(getActivity(),R.layout.listview_item_biz, null);
+
+                viewHolder = new ViewHolder();
+                viewHolder.name = (TextView) convertView.findViewById(R.id.tv_list_item_biz_name);
+                viewHolder.address = (TextView)
+                        convertView.findViewById(R.id.tv_list_item_biz_address);
+
+                convertView.setTag(viewHolder);
+            }
+            else
+            {
+                viewHolder = (ViewHolder)convertView.getTag();
+            }
+
+            viewHolder.name.setText(mBusinessArrayList.get(position).getmName());
+            viewHolder.address.setText(mBusinessArrayList.get(position).getmAddress());
+
+            return convertView;
         }
     }
+
+    private static class ViewHolder
+    {
+        public TextView name;
+        public TextView address;
+
+    }
+
 }
